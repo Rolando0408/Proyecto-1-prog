eca02fd787d464a258a3f3fc0a98185e2645b64e
Rolando Rivas <rolandorafaelrivas@gmail.com>
2023-10-28 18:49:25 -0400

    Iniciando programa y agregando validacion de opcion ingresada

--- Teleferico.pas
+++ Teleferico.pas
@@ -1,10 +1,19 @@
-
 Program teleferico;
-
 Uses crt;
-
 Var 
-  user,a: string;
-Begin
+  opc: integer;
+Begin//Begin principal
+repeat//Para validar la opcion
+	writeln('Bienvenido al teleferico de Merida, a continuacion seleccione la opcion que desea realizar.');
+	writeln('1. Comprar boleto');
+	writeln('2. Ver sistema del teleferico');
+	writeln('3. Salir');
+	readln(opc);
+	
+		if (opc < 1) or (opc > 3) then
+		begin//Begin del if para validar opcion
+			writeln('Opcion invalida. Por favor ingrese una opcion valida.');
+		end;//End del if para validar la opcion
+until (opc >= 1) and (opc <= 3);//Fin del repeat para validar opcion
 
-End.
+End.//end principal
